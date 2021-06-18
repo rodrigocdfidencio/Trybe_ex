249d61ef07e5f634c0119f3f8445d52aab309e4c
Rodrigo <rodrigo.fidencio@gmail.com>
2021-06-18 18:14:28 -0300

    Bloco 7 exercicios resolvidos

--- Trybewarts/script.js
+++ Trybewarts/script.js
@@ -1,14 +1,14 @@
-const check = document.getElementById('agreement');
-const submit = document.getElementById('submit-btn');
+const check = document.getElementById("agreement");
+const submit = document.getElementById("submit-btn");
 
-const counter = document.getElementById('counter');
-const textarea = document.getElementById('textarea');
+const counter = document.getElementById("counter");
+const textarea = document.getElementById("textarea");
 
-const login = document.getElementById('login');
-const password = document.getElementById('password');
-const loginButton = document.getElementById('login-button');
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
 
-const form = document.getElementById('evaluation-form');
+const form = document.getElementById("evaluation-form");
 
 function checkTerms() {
   submit.disabled = !check.checked;
@@ -19,10 +19,13 @@ function counterEvent() {
 }
 
 function submitLogin() {
-  if (login.value.toLowerCase() === 'tryber@betrybe.com' || password.value === '123456') {
-    alert('Olá, Tryber!');
+  if (
+    login.value.toLowerCase() === "tryber@betrybe.com" ||
+    password.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
   } else {
-    alert('Login ou senha inválidos.');
+    alert("Login ou senha inválidos.");
   }
 }
 
@@ -33,34 +36,42 @@ function deleteForms() {
 }
 
 function createParagraphs(paragraphs) {
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
-  paragraphs.appendChild(document.createElement('p'));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
+  paragraphs.appendChild(document.createElement("p"));
 }
 
 function appendFormsItens(formsItems) {
-  document.getElementsByTagName('p')[0].innerText = `Nome: ${formsItems[0]} ${formsItems[1]}`;
-  document.getElementsByTagName('p')[1].innerText = `Email: ${formsItems[2]}`;
-  document.getElementsByTagName('p')[2].innerText = `Casa: ${formsItems[3]}`;
-  document.getElementsByTagName('p')[3].innerText = `Família: ${formsItems[4]}`;
-  document.getElementsByTagName('p')[4].innerText = `Matérias: ${formsItems[5]}`;
-  document.getElementsByTagName('p')[5].innerText = `Avaliação: ${formsItems[6]}`;
-  document.getElementsByTagName('p')[6].innerText = `Observações: ${formsItems[7]}`;
+  document.getElementsByTagName(
+    "p"
+  )[0].innerText = `Nome: ${formsItems[0]} ${formsItems[1]}`;
+  document.getElementsByTagName("p")[1].innerText = `Email: ${formsItems[2]}`;
+  document.getElementsByTagName("p")[2].innerText = `Casa: ${formsItems[3]}`;
+  document.getElementsByTagName("p")[3].innerText = `Família: ${formsItems[4]}`;
+  document.getElementsByTagName(
+    "p"
+  )[4].innerText = `Matérias: ${formsItems[5]}`;
+  document.getElementsByTagName(
+    "p"
+  )[5].innerText = `Avaliação: ${formsItems[6]}`;
+  document.getElementsByTagName(
+    "p"
+  )[6].innerText = `Observações: ${formsItems[7]}`;
 }
 
 function createParagraphsSection(formsItems) {
-  const paragraphs = document.createElement('section');
+  const paragraphs = document.createElement("section");
   form.insertBefore(paragraphs, form.firstChild);
   createParagraphs(paragraphs);
   appendFormsItens(formsItems);
 }
 
 function makeSubjectsIntoString() {
-  const subjects = document.querySelectorAll('input[class=subject]:checked');
+  const subjects = document.querySelectorAll("input[class=subject]:checked");
   const subjectsAl = [];
   for (let index = 0; index < subjects.length; index += 1) {
     subjectsAl[index] = ` ${subjects[index].value}`;
@@ -70,14 +81,14 @@ function makeSubjectsIntoString() {
 
 function makeFormsVariables() {
   return [
-    document.getElementById('input-name').value,
-    document.getElementById('input-lastname').value,
-    document.getElementById('input-email').value,
-    document.getElementById('house').value,
-    document.querySelector('input[name=family]:checked').value,
+    document.getElementById("input-name").value,
+    document.getElementById("input-lastname").value,
+    document.getElementById("input-email").value,
+    document.getElementById("house").value,
+    document.querySelector("input[name=family]:checked").value,
     makeSubjectsIntoString(),
     document.querySelector('input[name="rate"]:checked').value,
-    document.getElementById('textarea').value,
+    document.getElementById("textarea").value,
   ];
 }
 function submitForms(event) {
@@ -87,12 +98,12 @@ function submitForms(event) {
   createParagraphsSection(formsItems);
 }
 
-loginButton.addEventListener('click', submitLogin);
+loginButton.addEventListener("click", submitLogin);
 
-textarea.addEventListener('keyup', counterEvent);
+textarea.addEventListener("keyup", counterEvent);
 
-check.addEventListener('change', checkTerms);
+check.addEventListener("change", checkTerms);
 
-submit.addEventListener('click', submitForms);
+submit.addEventListener("click", submitForms);
 
 window.onload = checkTerms;
--- exercises/Fundamentos Desenvolvimento Web/bloco_7/7.1/script.js
+++ exercises/Fundamentos Desenvolvimento Web/bloco_7/7.1/script.js
@@ -9,6 +9,11 @@ const clickCounter = event => {
 
 clicks.addEventListener('click', clickCounter);
 
+/* Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+Exemplo:
+String determinada: "Tryber x aqui!"
+Parâmetro: "Bebeto"
+Retorno: "Tryber Bebeto aqui!" */
 
 exemplo = "Tryber x aqui!";
 let param = 'Bebeto'
